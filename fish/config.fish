58df10323a7667a244ca38a02c9af622c09b0e47
Eli Mallon <eli.mallon@csats.com>
2015-10-06 20:56:44 -0700

    work laptop

--- fish/config.fish
+++ fish/config.fish
@@ -28,3 +28,23 @@ alias c "cd /Users/eli/code"
 alias http "python -m SimpleHTTPServer 8000"
 alias kube kubectl
 alias kubename "kubectl config use-context"
+
+# grc colourify stuff
+alias colourify "grc -es --colour=auto"
+alias configure 'colourify ./configure'
+alias diff 'colourify diff'
+alias make 'colourify make'
+alias gcc 'colourify gcc'
+alias as 'colourify as'
+alias gas 'colourify gas'
+alias ld 'colourify ld'
+alias netstat 'colourify netstat'
+alias ping 'colourify ping'
+alias traceroute 'colourify /usr/sbin/traceroute'
+alias head 'colourify head'
+alias tail 'colourify tail'
+alias dig 'colourify dig'
+alias mount 'colourify mount'
+alias ps 'colourify ps'
+alias mtr 'colourify mtr'
+alias df 'colourify df'
